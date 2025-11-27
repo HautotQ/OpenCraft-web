235dea212fb76bc5fe36ed940b52b28a532312b4
Tabarcraft <166629849+HautotQ@users.noreply.github.com>
2025-11-27 08:34:31 +0100

    Create EndView class to display end screen

---
+++ EndView.js
@@ -0,0 +1,20 @@
+// EndView.js
+
+class EndView {
+    
+    show(root) {
+        root.innerHTML = "";
+        
+        const container = document.createElement("div");
+        
+        const title = document.createElement("h2");
+        title.innerText = "Félicitations !";
+        container.appendChild(title);
+        
+        const text = document.createElement("h4");
+        text.innerText = "Vous pouvez rejouer à tout moment en recliquant sur Jouer les Questions.";
+        container.appendChild(text);
+        
+        root.appendChild(container);
+    }
+}
