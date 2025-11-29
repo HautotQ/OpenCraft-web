184ea3c5147be80bc5f5e0c34f9db0ea0d25b749
Tabarcraft <166629849+HautotQ@users.noreply.github.com>
2025-11-29 15:41:08 +0100

    Update ViewQuestionsView.js

--- ViewQuestionsView.js
+++ ViewQuestionsView.js
@@ -9,7 +9,7 @@ class ViewQuestionsView {
         this.target = target;
         target.innerHTML = "";
 
-        target.style.marginTop = "50px";
+        target.style.marginTop = "30px";
         
         const div = document.createElement("div");
         
